e140265696266af497b1b5ba7cf048f3f5a9910e
Emmanuel Barrera <barreraslzr@gmail.com>
2024-12-28 20:45:45 -0600

    Update of platformURLs.ts

--- libs/platformURLs.ts
+++ libs/platformURLs.ts
@@ -1,7 +1,5 @@
 export const platformUrls: { [key: string]: string } = {
-    telegram: 'https://t.me/your_telegram_channel',
     github: 'https://github.com/your_github_profile',
-    bluesky: 'https://bsky.app/profile/your_bluesky_handle',
     x: 'https://x.com/your_x_handle',
     instagram: 'https://www.instagram.com/your_instagram_handle',
 }
